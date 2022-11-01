78f15d913ef3b565f5325d5dadf9b8c48c9342e1
SMiles02 <36670125+SMiles02@users.noreply.github.com>
2022-11-01 01:30:38 +0000

    Update companyqueries1.cpp
    Updated solution to Company Queries 1 since the previous one was quite ugly. It also verifies the earlier pull request for binary_lifting.cpp
    
    Code tested at https://cses.fi/paste/783b7589f126906a4add61/

--- CSES/Problemset/Trees/companyqueries1.cpp
+++ CSES/Problemset/Trees/companyqueries1.cpp
@@ -1,47 +1,51 @@
 #include <bits/stdc++.h>
-#define ll long long
-#define sz(x) (int)(x).size()
 using namespace std;
 
-int n,q,v,k,up[200001][19];
-vector<int> children[200001];
-
-void dfs(int c, int p)
-{
-    up[c][0]=p;
-    for (int i=1;i<19;++i)
-        up[c][i]=up[up[c][i-1]][i-1];
-    for (int i : children[c])
-        dfs(i,c);
-}
-
-int lift(int x, int y)
-{
-    for (int i=0;i<19;++i)
-        if (((1<<i)&y))
-        {
-            x=up[x][i];
-            y-=(1<<i);
-        }
-    if (x==0)
-        return -1;
-    return x;
-}
-
-int main()
-{
-    ios_base::sync_with_stdio(0); cin.tie(0);
-    cin>>n>>q;
-    for (int i=2;i<=n;++i)
-    {
-        cin>>v;
-        children[v].push_back(i);
+struct BinaryLifting {
+    int N, L;
+    vector<vector<int>> up, e;
+    BinaryLifting(int n) : N(n), L((int)log2(n + 1) + 1), e(n + 1), up((int)log2(n + 1) + 1, vector<int>(n + 1)) {}
+    void add_directed_edge(int x, int y) {
+        e[x].push_back(y);
     }
-    dfs(1,0);
-    while (q--)
-    {
-        cin>>v>>k;
-        cout<<lift(v,k)<<"\n";
+    void add_undirected_edge(int x, int y) {
+        e[x].push_back(y);
+        e[y].push_back(x);
+    }
+    void build_lift(int c, int p) {
+        up[0][c] = p;
+        for (int i = 1; i < L; ++i)
+            up[i][c] = up[i - 1][up[i - 1][c]];
+        for (int i : e[c])
+            build_lift(i, c);
+    }
+    void build_lift(int root = 1) {
+        build_lift(root, 0);
+    }
+    int query(int x, int y) {
+        for (int i = 0; i < L; ++i)
+            if (y & (1 << i))
+                x = up[i][x];
+        return x;
+    }
+};
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    int n, q, p, x, y, ans;
+    cin >> n >> q;
+    BinaryLifting helper(n);
+    for (int i = 2; i <= n; ++i) {
+        cin >> p;
+        helper.add_directed_edge(p, i);
+    }
+    helper.build_lift();
+    while (q--) {
+        cin >> x >> y;
+        ans = helper.query(x, y);
+        if (ans == 0)
+            ans = -1;
+        cout << ans << "\n";
     }
     return 0;
 }
